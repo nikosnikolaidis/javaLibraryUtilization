886fcb518a86f02d79a07718ec917b4f6733482f
MariaKolyda <iis20004@uom.edu.gr>
2023-03-04 09:37:35 +0200

    Changes CheckerForMultiplePoms and List allTheFilesForAnalysis

--- src/main/java/javaLibraryUtilization/control/HelloService.java
+++ src/main/java/javaLibraryUtilization/control/HelloService.java
@@ -3,27 +3,22 @@ package javaLibraryUtilization.control;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
-
 import org.springframework.stereotype.Service;
-import domain.Project;
 import utils.Commands;
 
 @Service
 public class HelloService {
-
     public static List<String> librariesWithProblem = new ArrayList<>();
     public static StartAnalysis startAnalysis = new StartAnalysis();
     public static String home;
-    public static String current;
-    public static List<String> allTheFilesForAnalysis = new ArrayList<>();
-
     public void projectAnalysis(String projectURLfromEndpoint) throws IOException {
 
         home = System.getProperty("user.home");
         String projectName= projectURLfromEndpoint.split("/")[projectURLfromEndpoint.split("/").length-1].replace(".git", "");
+        List<String>  allTheFilesForAnalysis = new ArrayList<>();
 
-        Commands.cloneProject(home +"\\project", projectURLfromEndpoint);
-        checkerForMultiplePoms(home +"\\project\\" + projectName);
+        Commands.cloneProject(home +"\\eclipse-workspace\\project", projectURLfromEndpoint);
+        checkerForMultiplePoms(home +"\\eclipse-workspace\\project\\" + projectName,allTheFilesForAnalysis);
 
         System.out.println("Hello " +allTheFilesForAnalysis);
          for ( String s : allTheFilesForAnalysis) {
@@ -34,21 +29,29 @@ public class HelloService {
         	System.out.println("Libraries with errors: ");
         	librariesWithProblem.forEach(System.out::println);
         }
-       Commands.deleteProject(home+"\\project",projectName);
+        allTheFilesForAnalysis.clear();
+        Commands.deleteProject(home+"\\eclipse-workspace\\project",projectName);
     }
-    public void checkerForMultiplePoms(String path){
+    public void checkerForMultiplePoms(String path,List<String> allTheFilesForAnalysis) {
 
+        int add=0;
         File f = new File (path);
         File[] files=f.listFiles();
         for (File file : files) {
-                if (file.isDirectory()){
-                    checkerForMultiplePoms(file.toString());
+                if (file.isDirectory()) {
+                    checkerForMultiplePoms(file.toString(),allTheFilesForAnalysis);
+                    if  (file.getName().equals("src")) {
+                       add++;
+                    }
                 }
                 if (file.isFile()) {
                     if  (file.toString().equals(path +"\\pom.xml")) {
-                        allTheFilesForAnalysis.add(path);
+                        add++;
                     }
                 }
+        }
+        if  (add==2){
+            allTheFilesForAnalysis.add(path);
         }
     }
 }
